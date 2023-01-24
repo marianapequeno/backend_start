06f09581c00f96adaf3f6e7be5c2719c924af901
marianapequeno <marianapqueno@gmail.com>
2023-01-24 12:12:22 -0300

    Genero Service e Genero Repository extends

--- src/main/java/com/capgemini/start/domain/service/GeneroService.java
+++ src/main/java/com/capgemini/start/domain/service/GeneroService.java
@@ -1,17 +1,20 @@
 package com.capgemini.start.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import com.capgemini.start.domain.entity.Genero;
+import com.capgemini.start.domain.repository.GeneroRepository;
 
+@Service
 public class GeneroService extends AbstractService<Genero, Long> {
+	
+	@Autowired
+	private GeneroRepository repository;
 
 	@Override
 	protected JpaRepository<Genero, Long> getRepository() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.repository;
 	}
-
-	
-	
 }
