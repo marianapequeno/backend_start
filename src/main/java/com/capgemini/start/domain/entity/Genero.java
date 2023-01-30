2242e96af3cfc631df6e7ea2c9829b7b47eccc6b
marianapequeno <marianapqueno@gmail.com>
2023-01-30 10:20:55 -0300

    Bug na classe Genero

--- src/main/java/com/capgemini/start/domain/entity/Genero.java
+++ src/main/java/com/capgemini/start/domain/entity/Genero.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -16,21 +18,22 @@ import lombok.Setter;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "tb_genero")
+@Entity(name= "TB_GENERO")
 public class Genero {
 	
 	@EqualsAndHashCode.Include
 	@Id
-	@Column(name="ID_GENERO")
+	@Column(name="ID_GENERO", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name="DS_GENERO", nullable = false)
+	@Column(name="DS_GENERO", nullable = false, length=100)
 	private String descricao;
 	
 	@Column(name="DT_INCLUSAO", nullable = false)
 	private Date dataInclusao;
 	
-	@Column(name="DT_ALTERACAO", nullable = false)
+	@Column(name="DT_ALTERACAO")
 	private Date dataAlteracao;
 	
 }
